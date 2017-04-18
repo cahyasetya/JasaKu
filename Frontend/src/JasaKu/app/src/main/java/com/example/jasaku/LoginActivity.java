3c6856afb26047d8127548d8cf10eb8532e0abc8
raharj007 <rbuthorokunto@yahoo.com>
2017-04-18 17:50:57 +0700

    Nambah login-register

---
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.jasaku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
