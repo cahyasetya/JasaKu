dec54a6ed5788709130deae760d55637cd16b102
raharj007 <cahya.setyaa@gmail.com>
2017-04-18 22:49:33 +0700

    memperbaiki menu

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/MainActivity.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jasaku;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -84,17 +85,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.buat_toko) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.kelola_toko) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.profil) {
+            startActivity(new Intent(this,ProfileActivity.class));
+        } else if (id == R.id.keluar) {
 
         }
 
