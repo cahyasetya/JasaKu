cc4e55f4dcd18d7bb278195f29531f0c771ea780
raharj007 <cahya.setyaa@gmail.com>
2017-04-19 10:47:04 +0700

    menambahkan halaman detil pesanan

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/DetilPesananActivity.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/DetilPesananActivity.java
@@ -3,16 +3,31 @@ package com.example.jasaku;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.jasaku.adapter.DetilPesananAdapter;
+import com.example.jasaku.model.Belanjaan;
+import com.example.jasaku.model.Jasa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetilPesananActivity extends AppCompatActivity implements View.OnClickListener{
 
+    @BindView(R.id.detil_pesanan_recyclerview)
+    RecyclerView detilPesananRecyclerView;
     @BindView(R.id.lanjutkan)
     Button lanjutkanButton;
+
+    private LinearLayoutManager llm;
+    private DetilPesananAdapter adapter;
+    private List<Belanjaan> belanjaanList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +42,21 @@ public class DetilPesananActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void init(){
+        llm=new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
+        detilPesananRecyclerView.setLayoutManager(llm);
+        belanjaanList=new ArrayList<>();
+        Belanjaan belanjaan=new Belanjaan();
+        Jasa jasa=new Jasa();
+        jasa.setNama("Gayung");
+        jasa.setHarga(5000);
+        belanjaan.setJasa(jasa);
+        belanjaan.setKuantitas(1);
+        for(int i=0; i<10; i++){
+            belanjaanList.add(belanjaan);
+        }
+        adapter=new DetilPesananAdapter(this,belanjaanList);
+        detilPesananRecyclerView.setAdapter(adapter);
+
         lanjutkanButton.setOnClickListener(this);
     }
 
