a46d80368c010de3df31a390049cae1da2fbd0da
raharj007 <rbuthorokunto@yahoo.com>
2017-04-20 02:10:09 +0700

    Mengubah profile menjadi collapsing toolbar

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/ProfileActivity.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/ProfileActivity.java
@@ -1,15 +1,44 @@
 package com.example.jasaku;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.OnClick;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private CollapsingToolbarLayout collapsingToolbarLayout = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    //    ActionBar actionBar = getSupportActionBar();
+      //  actionBar.setDisplayHomeAsUpEnabled(true);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        collapsingToolbarLayout.setTitle(getResources().getString(R.string.user_name));
+
+        Context context = this;
+        collapsingToolbarLayout.setContentScrimColor(ContextCompat.getColor(context, R.color.colorPrimary));
+
+        toolbarTextAppearance();
+    }
+
+    private void toolbarTextAppearance(){
+        collapsingToolbarLayout.setCollapsedTitleTextAppearance(R.style.collapsedappbar);
+        collapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.expandedappbar);
     }
 }
