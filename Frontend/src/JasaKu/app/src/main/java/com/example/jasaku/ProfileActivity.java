54cdbc7e1bd16744355068858eab1c5edbf54c1a
raharj007 <rbuthorokunto@yahoo.com>
2017-04-20 02:25:01 +0700

    edit dkit2

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/ProfileActivity.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/ProfileActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 
 import butterknife.OnClick;
@@ -25,8 +24,6 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    //    ActionBar actionBar = getSupportActionBar();
-      //  actionBar.setDisplayHomeAsUpEnabled(true);
 
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
         collapsingToolbarLayout.setTitle(getResources().getString(R.string.user_name));
