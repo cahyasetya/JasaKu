f40b7f7b607e0809b115fc342bac14c1227c8080
raharj007 <cahya.setyaa@gmail.com>
2017-04-25 16:54:53 +0700

    menambahkan icon sort, filter, dan halaman untuk filter

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/fragment/HalamanUtamaFragment.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/fragment/HalamanUtamaFragment.java
@@ -1,13 +1,19 @@
 package com.example.jasaku.fragment;
 
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -28,6 +34,8 @@ public class HalamanUtamaFragment extends Fragment {
 
     @BindView(R.id.toko_recyclerview)
     RecyclerView tokoRecyclerView;
+    @BindView(R.id.toko_searchview)
+    SearchView tokoSearchview;
 
     private List<Toko> tokoList;
     TokoAdapter adapter;
@@ -35,6 +43,7 @@ public class HalamanUtamaFragment extends Fragment {
 
     public HalamanUtamaFragment() {
         // Required empty public constructor
+        setHasOptionsMenu(true);
     }
 
 
@@ -52,11 +61,17 @@ public class HalamanUtamaFragment extends Fragment {
     }
 
     private void init(){
+
+        tokoSearchview.onActionViewExpanded();
+        tokoSearchview.clearFocus();
+
         llm=new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
         tokoList=new ArrayList<>();
         Toko toko=new Toko();
         toko.setNama("Toko Ibu Bejo");
         toko.setDeskripsi("Menjual serabi khas Solo");
+        toko.setAlamat("Keputih Gg. 3f, Sukolilo, Surabaya");
+        toko.setHargaMinimal(5000);
         for(int i=0; i<10; i++){
             tokoList.add(toko);
         }
@@ -65,4 +80,21 @@ public class HalamanUtamaFragment extends Fragment {
         tokoRecyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.halaman_utama_menu,menu);
+
+        //custom sort icon
+        MenuItem sort=menu.findItem(R.id.sort);
+        Drawable sortIcon=sort.getIcon();
+        sortIcon.mutate().setColorFilter(ContextCompat.getColor(getContext(),R.color.white), PorterDuff.Mode.SRC_IN);
+        sort.setIcon(sortIcon);
+
+        //custom filter icon
+        MenuItem filter=menu.findItem(R.id.filter);
+        Drawable filterIcon=filter.getIcon();
+        filterIcon.mutate().setColorFilter(ContextCompat.getColor(getContext(),R.color.white),PorterDuff.Mode.SRC_IN);
+        filter.setIcon(filterIcon);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
