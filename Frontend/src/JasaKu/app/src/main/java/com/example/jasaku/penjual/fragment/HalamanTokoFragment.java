7c34e271b4982e06708c1d41147f1c6bd890b004
raharj007 <rbuthorokunto@yahoo.com>
2017-05-16 05:49:38 +0700

    edit halaman jasa & tambah upload gambar walopun blm jadi

--- Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/penjual/fragment/HalamanTokoFragment.java
+++ Frontend/src/JasaKu/app/src/main/java/com/example/jasaku/penjual/fragment/HalamanTokoFragment.java
@@ -1,25 +1,55 @@
 package com.example.jasaku.penjual.fragment;
 
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jasaku.R;
 import com.example.jasaku.model.Toko;
 
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.IOException;
+import java.util.UUID;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HalamanTokoFragment extends Fragment {
 
+    private static final String UPLOAD_URL = "www.google.com";
+    private static final int IMAGE_REQUEST_CODE = 3;
+    private static final int STORAGE_PERMISSION_CODE = 123;
+    private Bitmap bitmap;
+    private Uri filePath;
+
+    @BindView(R.id.imageViewToko)
+    ImageView profileImg;
     @BindView(R.id.textViewNamaToko)
     TextView namaTokoTextView;
     @BindView(R.id.editTextDescription)
@@ -30,6 +60,8 @@ public class HalamanTokoFragment extends Fragment {
     EditText kontakEditText;
     @BindView(R.id.editTextBuka)
     EditText jamOperasionalEditText;
+    @BindView(R.id.buttonEditGambar)
+    TextView editGambar;
 
     public HalamanTokoFragment() {
         // Required empty public constructor
@@ -46,6 +78,8 @@ public class HalamanTokoFragment extends Fragment {
 
         init();
 
+        requestStoragePermission();
+
         return view;
     }
 
@@ -56,6 +90,91 @@ public class HalamanTokoFragment extends Fragment {
         alamatEditText.setText(toko.getAlamat());
         kontakEditText.setText(toko.getKontak());
         jamOperasionalEditText.setText(toko.getJamOperasional());
+    }
+
+    @OnClick(R.id.buttonEditGambar)
+    public void getGambarFromGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Complete action using"), IMAGE_REQUEST_CODE);
+        uploadMultipart();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), filePath);
+                profileImg.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void uploadMultipart() {
+        String path = getPath(filePath);
+
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            new MultipartUploadRequest(getContext(), uploadId, UPLOAD_URL)
+                    .addFileToUpload(path, "image")
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload();
+        }catch (Exception exc){
+            Toast.makeText(getContext(), exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String getPath(Uri uri) {
+        Cursor cursor = getContext().getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContext().getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(getContext(), "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(getContext(), "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
 }
